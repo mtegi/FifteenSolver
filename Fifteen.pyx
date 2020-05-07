bae6217de246bd7d35d1d997e8cc4eb92bd04bb8
Etahel <44235258+Etahel@users.noreply.github.com>
2020-05-07 13:26:08 +0200

    reading data from files, fix to statistics output

--- Fifteen.pyx
+++ Fifteen.pyx
@@ -68,7 +68,7 @@ cpdef save_to_file(result_file: str, stats_file: str, result: Tuple[str, int, in
     f.write("".join([str(len(result[0])), '\n', result[0]]))
     f.close()
     f = open(stats_file, 'w')
-    concat = [str(len(result[0])), '\n', str(result[2]), '\n', str(result[2]), '\n',
+    concat = [str(len(result[0])), '\n', str(result[2]), '\n',
               str(result[3]), '\n', str(result[1]), '\n', format(time_lapsed,'.3f')]
     f.write("".join(concat))
     f.close()
--- plot.py
+++ plot.py
@@ -1,29 +1,103 @@
 import os
+import numpy as np
 import matplotlib.pyplot as plt
 
 search_orders = ["rdul", "rdlu", "drul", "drlu", "ludr", "lurd", "uldr", "ulrd"]
+heuristics = ["hamm", "manh"]
+levels = [1, 2, 3, 4, 5, 6, 7]
+data_categories = ["res_len", "visited_states", "computed_states", "max_depth", "time"]
+content = ["data_raw", "mean"]
 PUZZLE_DIR = "./puzzles"
 OUTPUT_DIR = "./output"
-STATISTICS_DIR = "./statistics"
-
-data = {}
-
-
-def read_file(file):
-    f = open(file, 'r')
-    lines = f.readlines()
-    f.close()
-    return int(lines[0]), int(lines[1]), int(lines[2]), int(lines[3]), int(lines[4]), float(lines[5])
+STATISTICS_BFS = "./statistics/bfs"
+STATISTICS_ASTR = "./statistics/astr"
 
 
-def read_data():
-    values = []
-    for o in search_orders:
-        files = [f for f in os.listdir("".join([STATISTICS_DIR, '/', o])) if f.endswith(".txt")]
-        for f in files:
-            values.append(read_file("".join([STATISTICS_DIR, '/', o, '/', f])))
-        data[o] = values
+def getFilesDepth(filename):
+    return int(filename.split("_")[1])
 
 
-read_data()
-print('done')
+def printDict(dictionary):
+    for strategy in dictionary:
+        print("@Data for strategy: " + strategy)
+        for level in dictionary[strategy]:
+            print("###level...: " + str(level))
+            for data_category in dictionary[strategy][level]:
+                print("---category data...: " + data_category)
+                print(dictionary[strategy][level][data_category]["data"])
+                print("---mean...: " + data_category)
+                print(dictionary[strategy][level][data_category]["mean"])
+
+
+def createDictionary(strategies):
+    dictionary = dict.fromkeys(strategies)
+    for strategy in dictionary:
+        dictionary[strategy] = dict.fromkeys(levels)
+        for level in dictionary[strategy]:
+            dictionary[strategy][level] = dict.fromkeys(data_categories)
+            for data_category in dictionary[strategy][level]:
+                dictionary[strategy][level][data_category] = dict.fromkeys(["data", "mean"])
+                dictionary[strategy][level][data_category]["data"] = []
+    return dictionary
+
+
+def readData(dictionary, datafolder):
+    for strategy in os.listdir(datafolder):
+        path = datafolder + "/" + strategy
+        for file in os.listdir(path):
+            level = getFilesDepth(file)
+            open_file = open(path + "/" + file, "r")
+            for data_category in data_categories:
+                if data_category == "time":
+                    dictionary[strategy][level][data_category]["data"].append(float(open_file.readline()))
+                else:
+                    dictionary[strategy][level][data_category]["data"].append(int(open_file.readline()))
+
+
+def calculateMean(dictionary):
+    for strategy in dictionary:
+        for level in dictionary[strategy]:
+            for data_category in dictionary[strategy][level]:
+                dictionary[strategy][level][data_category]["mean"] = \
+                    np.mean(dictionary[strategy][level][data_category]["data"])
+
+
+bfs = createDictionary(search_orders)
+readData(bfs, STATISTICS_BFS)
+calculateMean(bfs)
+printDict(bfs)
+
+astr = createDictionary(heuristics)
+readData(astr, STATISTICS_ASTR)
+calculateMean(astr)
+printDict(astr)
+
+# data to plot
+n_groups = 4
+means_frank = (90, 55, 40, 65)
+means_guido = (85, 62, 54, 20)
+
+# create plot
+fig, ax = plt.subplots()
+index = np.arange(n_groups)
+bar_width = 0.35
+opacity = 0.8
+
+rects1 = plt.bar(index, means_frank, bar_width,
+                 alpha=opacity,
+                 color='b',
+                 label='Frank')
+
+rects2 = plt.bar(index + bar_width, means_guido, bar_width,
+                 alpha=opacity,
+                 color='g',
+                 label='Guido')
+
+plt.xlabel('Person')
+plt.ylabel('Scores')
+plt.title('Scores by person')
+plt.xticks(index + bar_width, ('A', 'B', 'C', 'D'))
+plt.legend()
+
+plt.tight_layout()
+plt.show()
